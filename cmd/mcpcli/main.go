0e6c8798e11635162bf3c69959107f03f0c9d351
Ahmed R. Awadallah <ahmed@awadallah.ca>
2025-07-29 10:48:59 -0400

    Fix sort import, bump version to v0.4.2, add coverage

--- cmd/mcpcli/main.go
+++ cmd/mcpcli/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // version is the current release tag for the CLI.
-var version = "0.4.1"
+var version = "0.4.2"
 
 func main() {
 	// Create the root command
--- internal/generators/generator_table_test.go
+++ internal/generators/generator_table_test.go
@@ -1,6 +1,7 @@
 package generators
 
 import (
+	"sort"
 	"testing"
 )
 
